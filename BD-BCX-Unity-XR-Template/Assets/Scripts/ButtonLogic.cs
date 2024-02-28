3c9cdc3acc1b1bc310ec6c7a65d4573533e04365
juliusvinnen <julius.vinnen@inside-m2m.de>
2024-02-28 10:32:52 +0100

    Modify Unity Prject

--- BD-BCX-Unity-XR-Template/Assets/Scripts/ButtonLogic.cs
+++ BD-BCX-Unity-XR-Template/Assets/Scripts/ButtonLogic.cs
@@ -10,6 +10,12 @@ public class ButtonLogic : MonoBehaviour
     public String PayloadVar;
     public M2MqttUnityTest client;
     public bool isConnectButton;
+    public bool isPistonButton;
+    public bool isBeltButton;
+    public MachineModelBehaviour machineModelBehaviour;
+
+    public bool testIsRunning = false;
+    public float testStartedAt;
     
     
 
@@ -32,15 +38,36 @@ public class ButtonLogic : MonoBehaviour
                 client.SetBrokerPort("1883");
                 client.Connect();
             }
-            else
+            else if(isPistonButton)
             {
+                if(PayloadVar == "true")
+                {
+                    machineModelBehaviour.extendPiston = true;
+                }
+                else
+                {
+                    machineModelBehaviour.extendPiston = false;
+                }
                 client.PublishPayload(PayloadVar);
+            }
+            else if(isBeltButton)
+            {
+                testIsRunning = true;
+                testStartedAt = Time.time;
             }
 
             //client.TestPublish();
             //this.gameObject.SetActive(false);
         }
-   
+        if (testIsRunning)
+        {
+            if (Time.time - testStartedAt > 2)
+            {
+                machineModelBehaviour.highlightBelt = true;
+                testIsRunning = false;
+            }
+        }
+
     }
 
 }
